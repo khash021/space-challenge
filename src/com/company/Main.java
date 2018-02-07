cfd5f8a3ddde7c7d9326a6e94205f4678100dcfe
Khash <khash.021@gmail.com>
2018-02-07 13:55:04 -0800

    Add print function

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 public class Main {
 
+    public static void print(String s) {
+        System.out.println(s);
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
