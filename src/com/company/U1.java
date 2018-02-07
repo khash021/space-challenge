1c2511bfa468c9c56f68916c9de4418f98d63c16
Khash <khash.021@gmail.com>
2018-02-07 15:13:48 -0800

    Add fields for U1 and U2 classes

--- src/com/company/U1.java
+++ src/com/company/U1.java
@@ -2,6 +2,12 @@ package com.company;
 
 public class U1 extends Rocket {
 
-    //
+    //Fields. All weights are in tonnes, and cost in Million $
+    final int cost = 100;
+    final int rocketWeight = 10;
+    final int maxWeight = 18;
+    //maxCargo is the maximum weight of the cargo
+    final int maxCargo = maxWeight - rocketWeight;
+
 
 }
--- src/com/company/U2.java
+++ src/com/company/U2.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class U2 extends Rocket {
 
-    //
+    //Fields. All weights are in tonnes, and cost in Million $
+    final int cost = 200;
+    final int rocketWeight = 18;
+    final int maxWeight = 29;
+    //maxCargo is the maximum weight of the cargo
+    final int maxCargo = maxWeight - rocketWeight;
 
 }
