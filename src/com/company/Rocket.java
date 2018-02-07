c9f7452068df4427a10ec2bc8081b9d8851005b0
Khash <khash.021@gmail.com>
2018-02-07 14:36:46 -0800

    Add Rocket class

---
+++ src/com/company/Rocket.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Rocket implements SpaceShip {
+
+
+    public boolean launch() {
+        return true;
+    }
+
+    public boolean land(){
+        return true;
+    }
+
+    public boolean canCarry(Item i){
+        //will be updated later
+        return true;
+    }
+
+    public void carry(Item i){
+        //will be updated later
+    }
+
+}
