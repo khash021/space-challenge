8d6f533a98beaf9149160e63dcc1714032b92d35
Khash <khash.021@gmail.com>
2018-02-07 15:08:54 -0800

    Add Item class constructor

--- src/com/company/Item.java
+++ src/com/company/Item.java
@@ -1,6 +1,19 @@
 package com.company;
 
 public class Item {
+
     String name;
     int weight;
+
+    //Default constructor
+    public void Item(){}
+
+    //Constructor with two input args
+    public void Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
 }
+
+
