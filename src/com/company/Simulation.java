db9f5dea9995df07974078e65efd3c176a1b7b44
Khash <khash.021@gmail.com>
2018-02-09 14:17:45 -0800

    Add runSimulation method.

--- src/com/company/Simulation.java
+++ src/com/company/Simulation.java
@@ -15,7 +15,7 @@ public class Simulation  {
     private ArrayList<U1> loadedU1Array = new ArrayList<>();
     private ArrayList<U2> loadedU2Array = new ArrayList<>();
 
-    private int cost = 0;
+    private int totalCost = 0;
 
     public Simulation() throws Exception{}
 
@@ -46,8 +46,7 @@ public class Simulation  {
     public ArrayList<U1> loadU1 (ArrayList<Item> inputArray) {
         U1 u1 = new U1();
         while (inputArray.size() > 0) {
-            int size = inputArray.size();
-            for (int i=size; i>0 ; i--) {
+            for (int i=inputArray.size(); i>0 ; i--) {
                 Item item = inputArray.get(i-1);
                 if (u1.canCarry(item)) {
                     u1.carry(item);
@@ -64,8 +63,7 @@ public class Simulation  {
     public ArrayList<U2> loadU2 (ArrayList<Item> inputArray) {
         U2 u2 = new U2();
         while (inputArray.size() > 0) {
-            int size = inputArray.size();
-            for (int i=size; i>0 ; i--) {
+            for (int i=inputArray.size(); i>0 ; i--) {
                 Item item = inputArray.get(i-1);
                 if (u2.canCarry(item)) {
                     u2.carry(item);
@@ -78,4 +76,65 @@ public class Simulation  {
         return loadedU2Array;
     } //loadU1
 
-}
+
+    public int runSimulation (String rocketType, int phase) throws Exception {
+        //Determining the phase
+        String fileName;
+        switch (phase) {
+            case 1:
+                fileName = "phase-1.txt";
+                break;
+            case 2:
+                fileName = "phase-2.txt";
+                break;
+            default:
+                print("Invalid input (possible mis-spelling)");
+                return -1;
+        } //switch
+
+        ArrayList<Item> itemList = this.loadItems(fileName);
+
+        //Determining the ship type for simulation
+        switch (rocketType) {
+            case "u1": case "U1":
+            U1 u1 = new U1();
+            ArrayList<U1> U1Array = this.loadU1(itemList);
+
+            while (U1Array.size() > 0) {
+                for (int i = U1Array.size(); i>0; i--) {
+                    u1 = U1Array.get(i - 1);
+                    int counter = 1;
+                    while (!(u1.land(u1.currentCargo) && u1.launch(u1.currentCargo))) {
+                        counter++;
+                    } //while
+                    totalCost += u1.cost * counter;
+                    U1Array.remove(i-1);
+                } //if
+            } //while
+            return totalCost;
+
+            case "u2": case "U2":
+            U2 u2 = new U2();
+            ArrayList<U2> U2Array = this.loadU2(itemList);
+
+            while (U2Array.size() > 0) {
+                for (int i = U2Array.size(); i>0; i--) {
+                    u2 = U2Array.get(i - 1);
+                    int counter = 1;
+                    while (!(u2.land(u2.currentCargo) && u2.launch(u2.currentCargo))) {
+                        counter++;
+                    } //while
+                    totalCost += u2.cost * counter;
+                    U2Array.remove(i-1);
+                } //if
+            } //while
+            return totalCost;
+
+            default:
+            print("Invalid input (possible mis-spelling)");
+            return -1; //We can use -1 in the method call in Main to handle all invalid inputs since cost can never be negative
+        } //switch
+
+    } //runSimulation
+
+} //main
