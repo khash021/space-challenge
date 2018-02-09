1f52cc791a57cb49dced5e0642d61fe141fac2f9
Khash <khash.021@gmail.com>
2018-02-09 14:16:57 -0800

    Add Simulation object to run simulation and return the total cost

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,18 @@ package com.company;
 
 public class Main {
 
+
     public static void print(String s) {
         System.out.println(s);
     }
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws Exception {
+
+        String rocket = "u1";
+        int phase = 2;
+        Simulation simulation = new Simulation();
+        print("Phase: " + phase + "\nRocket: " + rocket + "\nTotal cost of the mission: " +
+                simulation.runSimulation(rocket, phase) + " $ million");
+
     }
 }
