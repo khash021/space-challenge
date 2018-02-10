8f9d018c42502ce71bfe1429f5dca101216a427a
Khash <khash.021@gmail.com>
2018-02-09 16:34:23 -0800

    Add functions and input to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,47 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
 
     public static void print(String s) {
         System.out.println(s);
     }
 
-    public static void main(String[] args) throws Exception {
+    public static int simulate(int i) throws Exception {
+        Simulation s = new Simulation();
+        int cost=0;
+        switch (i){
+            case 1:
+                cost = s.runSimulation("u1", 1) + s.runSimulation("u1", 2);
+                break;
+            case 2:
+                cost = s.runSimulation("u2", 1) + s.runSimulation("u2", 2);
+                break;
+            default:
+                print("Invalid input");
+        }
+        return cost;
+    }
 
-        String rocket = "u1";
-        int phase = 2;
-        Simulation simulation = new Simulation();
-        print("Phase: " + phase + "\nRocket: " + rocket + "\nTotal cost of the mission: " +
-                simulation.runSimulation(rocket, phase) + " $ million");
+    public static void main(String[] args) throws Exception{
+
+        print("\nPlease select the rocket type by enter number \"1\" for U1 rocket, or \"2\" for U2 rocket" +
+                "\nEnter number");
+        Scanner scanner = new Scanner(System.in);
+        int type = scanner.nextInt();
+        String rocket = new String();
+        switch (type) {
+            case 1:
+                rocket = "U1";
+                break;
+            case 2:
+                rocket = "U2";
+                break;
+        }
+        int cost = simulate(type);
+        print("The total cost of the mission using \"" + rocket + "\" rocket is: " + cost + " $ millions.");
 
     }
 }
+
